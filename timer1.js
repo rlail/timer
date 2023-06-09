467922e9d60103c300b3a0c265cadafe6b2d37fa
Rajan Lail <rajanlail3993@gmail.com>
2023-06-09 13:58:12 -0700

    Add comments

--- timer1.js
+++ timer1.js
@@ -1,12 +1,12 @@
 const args = process.argv.slice(2);
 
-for (const arg of args) {
-  const number = Number(arg);
+for (const arg of args) {               //Iterate over each arg
+  const number = Number(arg);           //Convert each arg to a number 
 
-  if (!isNaN(number) && number >= 0) {
+  if (!isNaN(number) && number >= 0) {   //Check for NaN
     setTimeout(() => {
       process.stdout.write('\x07');
-    }, number * 1000);
+    }, number * 1000);                   //Change milliseconds to seconds 
   }
 }
 
